eacdd7400d1babec821cdea0ba84aee1c509d5e2
laaland3777-art <laaland3777@gmail.com>
2026-01-12 18:23:16 +0800

    app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ with col1:
     
     # 2. SMV adipose thickness
     f2 = st.number_input(
-        "ileocolic perivascular adipose thickness (mm)", 
+        "Ileocolic perivascular adipose thickness (mm)", 
         min_value=0.0, 
         value=10.0, 
         step=0.1,
@@ -134,5 +134,6 @@ if st.button("Predict Difficulty", type="primary"):
 # --- 页脚 ---
 st.markdown("---")
 st.caption("Model based on Weighted Ensemble (LR, AdaBoost, SVM, MLP, XGBoost).")
+
 
 
